b6f788df3eb3c700b39d3da13cf552b938073ee6
Torrez, Milton N <torrez.mn@gmail.com>
2021-07-06 20:09:11 +0200

    -

---
+++ SChool_Admin_App/app_administration/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppAdministrationConfig(AppConfig):
+    name = 'app_administration'
---
+++ SChool_Admin_App/app_tutors/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppTutorsConfig(AppConfig):
+    name = 'app_tutors'
